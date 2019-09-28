f7ea7ab3c37dbc31f1d4b60e73b6583a896d1767
aishans <alshaijiaisha@gmail.com>
2019-09-28 12:31:33 +0300

    ais's last changes

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -1,33 +1,37 @@
-  
 import React, { Component } from "react";
 import LogoutButton from "../Buttons/LogoutButton";
 import { observer } from "mobx-react";
 
 // NativeBase Components
-import { Card, CardItem, Text, Button, Spinner, List, ListItem, Content } from "native-base";
+import {
+  Card,
+  CardItem,
+  Text,
+  Button,
+  Spinner,
+  List,
+  ListItem,
+  Content
+} from "native-base";
 //Stores
-import LogoutButton from "../Buttons/LogoutButton";
+
 import authStore from "../../stores/authStore";
 import corpseStore from "../../stores/corpseStore";
 
 const Profile = ({ navigation }) => {
   if (authStore.user) {
     corpseStore.OrderHistory();
-  }else{
+  } else {
     navigation.replace("Login");
-  return (
-    <Content>
-      <Card>
-        <CardItem>
-          <LogoutButton />
-        </CardItem>
-      </Card>
-    </Content>
-  );
+    return (
+      <Content>
+        <Card>
+          <CardItem>
+            <LogoutButton />
+          </CardItem>
+        </Card>
+      </Content>
+    );
+  }
 };
 export default observer(Profile);
-
-  
-
-
-

a54cbc96837a2d523912d1b7c29d02aed685ab37
aishans <alshaijiaisha@gmail.com>
2019-09-28 22:54:47 +0300

    no changes

--- components/CorpseDetail/index.js
+++ components/CorpseDetail/index.js
@@ -36,76 +36,96 @@ class CorpseDetail extends Component {
     const corpseID = navigation.getParam("corpseID");
     const corpse = corpses.find(corpse => corpse.id === corpseID);
     return (
-      <Content>
-        <List>
-          <ListItem style={styles.top}>
-            <Left>
-              <Text style={styles.text}>
-                {corpse.name + "\n"}
-                <Text note>{corpse.location}</Text>
-              </Text>
-            </Left>
-            <Body />
-            <Right>
-              <Thumbnail bordered source={{ uri: corpse.image }} />
-            </Right>
-          </ListItem>
-          <Button
-            full
-            danger
-            onPress={() =>
-              cartStore.addItemToCart({
-                ...corpse,
-                quantity: this.state.quantity
-              })
-            }
-          >
-            <Text>Add</Text>
-          </Button>
-          <Button
-            full
-            danger
-            onPress={() =>
-              cartStore.removeItemFromCart({
-                ...corpse,
-                quantity: this.state.quantity
-              })
-            }
-          >
-            <Text>Remove</Text>
-          </Button>
-        </List>
-        <Card style={styles.transparent}>
-          <CardItem style={styles.transparent}>
-            <Left />
-            <Thumbnail
-              bordered
-              source={{ uri: corpse.image }}
-              style={styles.thumbnail}
-            />
-            <Text style={styles.text}>{corpse.name}</Text>
-            <Text note style={styles.text}>
-              {corpse.blood_type}
-            </Text>
-            <Text note style={styles.text}>
-              {corpse.skin_color}
-            </Text>
-            <Text note style={styles.text}>
-              {corpse.amputation_date}
-            </Text>
-            <Text note style={styles.text}>
-              {corpse.expiary_date}
-            </Text>
-            <Text note style={styles.text}>
-              {corpse.reason_for_amputation}
-            </Text>
-          </CardItem>
-        </Card>
-      </Content>
-    );
-  }
-}
-
+//       <Content>
+//         <List>
+//           <ListItem style={styles.top}>
+//             <Left>
+//               <Text style={styles.text}>
+//                 {corpse.name + "\n"}
+//                 <Text note>{corpse.location}</Text>
+//               </Text>
+//             </Left>
+//             <Body />
+//             <Right>
+//               <Thumbnail bordered source={{ uri: corpse.image }} />
+//             </Right>
+//           </ListItem>
+//           <Button
+//             full
+//             danger
+//             onPress={() =>
+//               cartStore.addItemToCart({
+//                 ...corpse,
+//                 quantity: this.state.quantity
+//               })
+//             }
+//           >
+//             <Text>Add</Text>
+//           </Button>
+//           <Button
+//             full
+//             danger
+//             onPress={() =>
+//               cartStore.removeItemFromCart({
+//                 ...corpse,
+//                 quantity: this.state.quantity
+//               })
+//             }
+//           >
+//             <Text>Remove</Text>
+//           </Button>
+//         </List>
+//         <Card style={styles.transparent}>
+//           <CardItem style={styles.transparent}>
+//             <Left />
+//             <Thumbnail
+//               bordered
+//               source={{ uri: corpse.image }}
+//               style={styles.thumbnail}
+//             />
+//             <Text style={styles.text}>{corpse.name}</Text>
+//             <Text note style={styles.text}>
+//               {corpse.blood_type}
+//             </Text>
+//             <Text note style={styles.text}>
+//               {corpse.skin_color}
+//             </Text>
+//             <Text note style={styles.text}>
+//               {corpse.amputation_date}
+//             </Text>
+//             <Text note style={styles.text}>
+//               {corpse.expiary_date}
+//             </Text>
+//             <Text note style={styles.text}>
+//               {corpse.reason_for_amputation}
+//             </Text>
+//           </CardItem>
+//         </Card>
+//       </Content>
+//     );
+//   }
+// }
+<Container>
+<Content>
+  <View>
+    <Card>
+      <CardItem>
+        <Body>
+          <Image style={styles.icecreamImage} />
+          <Text>FLAVOR: </Text>
+        </Body>
+      </CardItem>
+    </Card>
+    <Button
+      title="BACK"
+      color="#6C788E"
+      onPress={() => alert("Come Baaack!")}
+    />
+  </View>
+</Content>
+</Container>
+);
+};
 CorpseDetail.navigationOptions = ({ navigation }) => ({
   title: navigation.getParam("CorpseName"),
   headerRight: <CartButton />
